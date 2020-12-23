129b1ca3022a13ac8ba7ef16b19d2985edcc4ade
Mohammad Mahdi Ramezanpour <i@ramezanpour.net>
2020-12-23 11:46:11 +0330

    First commit

---
+++ updateDotFiles.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This scripts is going to copy all files needed to be in my dotfiles into their directory.
+# After than it pushes the script into the git repository.
+
+DOTFILES=~/dev/dotfiles
+
+# ZSH
+cp $HOME/.zshrc $DOTFILES/.zshrc
+# Neovim
+mkdir -p $DOTFILES/neovim
+cp $HOME/.config/nvim/* $DOTFILES/neovim/
+# TMUX
+cp $HOME/.tmux.conf $DOTFILES/tmux.conf
+# VIM
+cp $HOME/.vimrc $DOTFILES/.vimrc
+
+# Newboat
+mkdir -p $DOTFILES/newsboat
+cp $HOME/.newsboat/* $DOTFILES/newsboat
+
+cd $DOTFILES
+
+git add .
+git commit -m "Update dotfiles"
+git push
