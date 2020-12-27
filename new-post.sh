b1fc8e2e518f284c7d351424993e3c38fbccce2e
Mohammad Mahdi Ramezanpour <i@ramezanpour.net>
2020-12-27 13:09:29 +0330

    Add new-post scripts for creating a new blog post

---
+++ new-post.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+    echo "Post name is required"
+    exit 1
+fi
+
+WORKDIR=~/dev/ramezanpour.net
+cd $WORKDIR
+POSTNAME=$1
+CATEGIORY=$2
+
+if [[$CATEGIORY == ""]]; then
+    CATEGIORY="misc"
+fi
+POSTFILE="${WORKDIR}/_posts/$(date +%F)-${POSTNAME// /-}.markdown"
+touch $POSTFILE
+echo "---
+title: ${POSTNAME}
+layout: post
+date: $(date +'%F %T')
+categories: ${CATEGIORY}
+---" > $POSTFILE
+nvim $POSTFILE
